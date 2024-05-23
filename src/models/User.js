538a6194679493595bab6e7c3c51b9283b8cb712
uxharshit <harshitkatheria7890@gmail.com>
2024-05-23 07:52:01 +0530

    Added timestamp

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email : { type: String, required: true, unique: true },
-});
+}, { timestamps: true });
 
 userSchema.pre('save', async function (next) {
     const user = this;
